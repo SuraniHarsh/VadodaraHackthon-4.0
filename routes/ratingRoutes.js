85b2e78a94a40213ec6918ca971ef66c2ac9e7fd
SuraniHarsh <harshsurani00@gmail.com>
2023-09-22 10:44:48 +0530

    null

--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -3,6 +3,6 @@ const validateTocken = require("../middleware/authMiddleware");
 const rating = require("../controllers/ratingContrillers");
 const route = express.Router();
 
-route.route("/").get(rating);
+route.route("/").post(validateTocken,rating);
 
 module.exports = route;
