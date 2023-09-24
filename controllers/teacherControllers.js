5d851e0b8df656780f4d0fbbf60724079d060a37
SuraniHarsh <harshsurani00@gmail.com>
2023-09-25 00:16:20 +0530

    Teacher Profile done

--- controllers/teacherControllers.js
+++ controllers/teacherControllers.js
@@ -1,12 +1,49 @@
 const asyncHandler = require("express-async-handler");
 const { getTeachersByLocationAndRating } = require("../utils/filterUtils");
+const TeacherProfile = require("../models/TeacherProfileSModel");
 
+const teacherProfile = asyncHandler(async (req, res) => {
+  try {
+
+    const userId = req.user._id;
+
+    const teacherProfile = await TeacherProfile.findOne({
+      user_id: userId,
+    }).populate('user_id');
+
+    if (!teacherProfile) {
+      res.status(404)
+      .json({
+         error: "Teacher profile not found"
+        });
+      return;
+    }
+
+    res.status(200).json(teacherProfile);
+
+  } catch (error) {
+    res.status(500)
+    .json({
+      message : error.message,
+      error : error
+    })
+  }
+});
+
+/**
+ * @desc Get teachers by location and rating
+ * @route GET /api/v1/teacher/suggestion
+ * @access Public
+ */
 const teachersByLocationAndRating = asyncHandler(async (req, res) => {
   try {
     const { location, page = 1, pageSize = 4 } = req.query;
 
     if (!location) {
-      return res.status(400).json({ error: "Location parameter is required" });
+      return res.status(400)
+              .json({
+                error: "Location parameter is required"
+                });
     }
 
     const teachersByRating = await getTeachersByLocationAndRating(
@@ -24,4 +61,5 @@ const teachersByLocationAndRating = asyncHandler(async (req, res) => {
 
 module.exports = {
   teachersByLocationAndRating,
+  teacherProfile
 };
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {
-  teachersByLocationAndRating,
+  teachersByLocationAndRating, teacherProfile,
 } = require("../controllers/teacherControllers");
+const validateTocken = require("../middleware/authMiddleware");
 const route = express.Router();
 
 route.route("/suggestion").get(teachersByLocationAndRating);
+route.route("/profile").get(validateTocken, teacherProfile);
 
 module.exports = route;
